b3ed8cc45bbc91d2fe18135fbd09b81f50e0f42b
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-21 13:57:06 -0400

    Add FibonacciNumber class
    Add FibonacciNumber class
    Use self.new instead of initialize as a method
    in order to return a number instead of an object
    Use recursion

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -3,6 +3,10 @@ class FibonacciRange
 
   attr_reader :stop
 
+  def initialize(stop)
+    @stop = stop
+  end
+
   def each
     previous_fib = 0
     current_fib = 0
@@ -11,18 +15,26 @@ class FibonacciRange
         yield current_fib
         current_fib += 1
       else
-        next_fib = previous_fib + current_fib
         yield current_fib
+        next_fib = previous_fib + current_fib
         previous_fib = current_fib
         current_fib = next_fib
       end
     end
   end
 
-  def initialize(stop)
-    @stop = stop
-  end
 end
 
 class FibonacciNumber
+  def self.new(position = 1)
+    if position < 1
+      "Position must be greater than 0"
+    elsif position == 1
+      0
+    elsif position == 2
+      1
+    else
+      FibonacciNumber.new(position - 1) + FibonacciNumber.new(position - 2)
+    end
+  end
 end
