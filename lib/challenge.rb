077f40b6d76a3ce7fb4bf66a1acb4552fcd62d52
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-21 13:26:50 -0400

    Add partial Fib Range class
    Add initialize method to FibonacciRange class
    Add partial each method that calculates 3rd number and beyond

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,36 @@
 class FibonacciRange
+  include Enumerable
+
+  attr_reader :stop
+
+  # def fib(current_fib)
+  #   if (0..1).include? current_fib 
+  #     return current_fib
+  #   return fib(current_fib - 1) + fib(current_fib - 2)
+  # end
+
+  # def each
+  #   current_fib = 0
+  #   while (current_fib < stop)
+  #       yield fib(current_fib)
+  #   end
+  # end
+
+  # as written, this yields the 3rd number & beyond of the fib sequence
+  def each
+    previous_fib = 0
+    current_fib = 1
+    while (current_fib <= stop)
+      next_fib = previous_fib + current_fib
+      yield next_fib
+      previous_fib = current_fib
+      current_fib = next_fib
+    end
+  end
+
+  def initialize(stop)
+    @stop = stop
+  end
 end
 
 class FibonacciNumber
