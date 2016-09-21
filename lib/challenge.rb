a08e6293086fa3145bf10fc3faeb76b49eda9786
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-21 13:34:36 -0400

    Add working each method to FibRange
    Add fully functional each method to FibonacciRange class

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -3,28 +3,19 @@ class FibonacciRange
 
   attr_reader :stop
 
-  # def fib(current_fib)
-  #   if (0..1).include? current_fib 
-  #     return current_fib
-  #   return fib(current_fib - 1) + fib(current_fib - 2)
-  # end
-
-  # def each
-  #   current_fib = 0
-  #   while (current_fib < stop)
-  #       yield fib(current_fib)
-  #   end
-  # end
-
-  # as written, this yields the 3rd number & beyond of the fib sequence
   def each
     previous_fib = 0
-    current_fib = 1
+    current_fib = 0
     while (current_fib <= stop)
-      next_fib = previous_fib + current_fib
-      yield next_fib
-      previous_fib = current_fib
-      current_fib = next_fib
+      if current_fib == 0
+        yield current_fib
+        current_fib += 1
+      else
+        next_fib = previous_fib + current_fib
+        yield current_fib
+        previous_fib = current_fib
+        current_fib = next_fib
+      end
     end
   end
 
